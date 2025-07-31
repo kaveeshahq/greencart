d26f36795411ba648e767ceafa97fdf54113565a
Kaveesha Dissanayake <90174106+kaveeshahq@users.noreply.github.com>
2025-07-31 11:03:05 +0530

    Create the Footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,24 @@
-import { Route, Routes, useLocation } from 'react-router-dom'
-import Navbar from './components/Navbar'
-import Home from './pages/Home'
-import { Toaster } from 'react-hot-toast'
+import { Route, Routes, useLocation } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import { Toaster } from "react-hot-toast";
+import Footer from "./components/Footer";
 const App = () => {
-
   const isSellerPath = useLocation().pathname.includes("seller");
   return (
     <div>
-
-      {isSellerPath ? null :<Navbar /> } 
-      <Toaster/>
-       <div  className ={`${ isSellerPath ? "" : "px-6 md:px-16 lg:px-24 xl:px-32"}`}>
+      {isSellerPath ? null : <Navbar />}
+      <Toaster />
+      <div
+        className={`${isSellerPath ? "" : "px-6 md:px-16 lg:px-24 xl:px-32"}`}
+      >
         <Routes>
-          <Route path='/' element={<Home/>} />
+          <Route path="/" element={<Home />} />
         </Routes>
-       </div>
+      </div>
+      {!isSellerPath && <Footer />}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;

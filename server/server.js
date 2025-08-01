0bbc392b0c42d80da420bdff8c0f1209260f1564
Kaveesha Dissanayake <90174106+kaveeshahq@users.noreply.github.com>
2025-08-01 17:02:12 +0530

    Create the folder structure of backend , create the server file , connect to mongodb , create usermodel

---
+++ server/server.js
@@ -0,0 +1,29 @@
+import cookieParser from 'cookie-parser';
+import express from 'express';
+import cors from 'cors';
+import connectDB from './configs/db.js';
+import 'dotenv/config'
+
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+await connectDB();
+
+// Allowed Origins
+
+const allowedOrigins =[
+    'http://localhost:5173'
+]
+
+
+// Middleware Configuration
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({origin: allowedOrigins, credentials: true}));
+
+app.get('/', (req,res) => res.send('API Working')); 
+
+app.listen(port, () =>{
+    console.log(`Server running on port ${port}`);
+})

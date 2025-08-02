f2c7835a398ac2ec4aeda8a927bdec61f48af445
Kaveesha Dissanayake <90174106+kaveeshahq@users.noreply.github.com>
2025-08-02 18:29:16 +0530

    Minor Change in Product Model

--- server/models/product.js
+++ server/models/product.js
@@ -26,7 +26,7 @@ const productSchema = new mongoose.Schema(
       type: Array,
       required: true,
     },
-    offinStockerPrice: {
+    inStock: {
       type: Boolean,
       default: true,
     },

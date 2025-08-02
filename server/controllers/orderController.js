aaba5f7f4a98da554ad0e487e6d7d25b5a25e01b
Kaveesha Dissanayake <90174106+kaveeshahq@users.noreply.github.com>
2025-08-02 23:32:53 +0530

    Stripe Gateway

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -1,11 +1,11 @@
 import Order from "../models/order.js";
 import Product from "../models/product.js";
+import stripe from "stripe";
 
 // Place Order COD : /api/order/cod
-
 export const placeOrderCOD = async (req, res) => {
   try {
-    const {  address, items } = req.body;
+    const { address, items } = req.body;
     const userId = req.userId;
     if (!address || items.length === 0) {
       return res.json({ success: false, message: "Invalid Data" });
@@ -36,11 +36,81 @@ export const placeOrderCOD = async (req, res) => {
   }
 };
 
-//  Get Orders by User ID : /api/order/user
+// Place Order Stripe : /api/order/stripe
+export const placeOrderStripe = async (req, res) => {
+  try {
+    const { address, items } = req.body;
+    const userId = req.userId;
+    const { origin } = req.headers;
+    if (!address || items.length === 0) {
+      return res.json({ success: false, message: "Invalid Data" });
+    }
 
+    let productData = [];
+
+    // Calculate amount using items
+
+    let amount = await items.reduce(async (acc, item) => {
+      const product = await Product.findById(item.product);
+      productData.push({
+        name: product.name,
+        price: product.offerPrice,
+        quantity: item.quantity,
+      });
+      return (await acc) + product.offerPrice * item.quantity;
+    }, 0);
+
+    // Add tax charge 2%
+
+    amount += Math.floor(amount * 0.02);
+
+    const order = await Order.create({
+      userId,
+      items,
+      amount,
+      address,
+      paymentType: "Online",
+    });
+
+    // Stripe Gateway
+    const stripeInstance = new stripe(process.env.STRIPE_SECRET_KEY);
+
+    // Create line items for stripe
+    const line_items = productData.map((item) => {
+      return {
+        price_data: {
+          currency: "usd",
+          product_data: {
+            name: item.name,
+          },
+          unit_amount: Math.floor(item.price + item.price * 0.02) * 100,
+        },
+        quantity: item.quantity,
+      };
+    });
+
+    // Create Session
+    const session = await stripeInstance.checkout.sessions.create({
+      line_items,
+      mode: "payment",
+      success_url: `${origin}/loader?next=my-orders`,
+      cancel_url: `${origin}/cart`,
+      metadata: {
+        orderId: order._id.toString(),
+        userId,
+      },
+    });
+
+    return res.json({ success: true, url: session.url });
+  } catch (error) {
+    return res.json({ success: false, message: error.message });
+  }
+};
+
+//  Get Orders by User ID : /api/order/user
 export const getUserOrders = async (req, res) => {
   try {
-const userId = req.userId;
+    const userId = req.userId;
     const orders = await Order.find({
       userId,
       $or: [{ paymentType: "COD" }, { isPaid: true }],
@@ -55,7 +125,6 @@ const userId = req.userId;
 };
 
 // Get All Orders for seller/dmin : /api/order/seller
-
 export const getAllOrders = async (req, res) => {
   try {
     const orders = await Order.find({
@@ -63,7 +132,7 @@ export const getAllOrders = async (req, res) => {
     })
       .populate("items.product address")
       .sort({ createdAt: -1 });
-       res.json({ success: true, orders });
+    res.json({ success: true, orders });
   } catch (error) {
     return res.json({ success: false, message: error.message });
   }

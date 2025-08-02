f8af8ab63ba2aeb066c04afe0d36a0107c628b4b
Kaveesha Dissanayake <90174106+kaveeshahq@users.noreply.github.com>
2025-08-02 15:26:32 +0530

    Create the Address model, controller,router

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ import sellerRouter from "./routes/sellerRoute.js";
 import connectCloudinary from "./configs/cloudinary.js";
 import productRouter from "./routes/productRoute.js";
 import cartRouter from "./routes/cartRoute.js";
+import addressRouter from "./routes/addressRoute.js";
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -31,6 +32,8 @@ app.use("/api/user", userRouter);
 app.use("/api/seller", sellerRouter);
 app.use("/api/product", productRouter);
 app.use("/api/cart", cartRouter);
+app.use("/api/address", addressRouter);
+
 
 
 app.listen(port, () => {
